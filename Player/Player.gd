be47518932182114948ff57908f6d7466b96615f
Austin Jones <jonesaustindev@gmail.com>
2023-02-25 15:42:14 -0600

    setup player scene and get background tilemap setup

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,14 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10
-const MAX_SPEED = 100
-const FRICTION = 10
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
 
 var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,10 +18,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	print(velocity)
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
