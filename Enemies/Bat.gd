917eea9ad32d0691299d8a5a6fb10210ef957c60
Austin Jones <jonesaustindev@gmail.com>
2023-02-28 17:13:46 -0600

    add enemy detection and chase

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,14 +1,53 @@
 extends KinematicBody2D
 
-const FRICTION = 200
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+export var ACCELERATION := 300
+export var FRICTION := 200
+export var MAX_SPEED := 50
+
+enum State {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
+var state = State.CHASE
+
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		State.IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		
+		State.WANDER:
+			pass
+		
+		State.CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = State.IDLE
+			sprite.flip_h = velocity.x < 0
+				
+	
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = State.CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
@@ -16,3 +55,6 @@ func _on_Hurtbox_area_entered(area):
 
 func _on_Stats_no_health():
 	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
