cc4bf6fc5665c8bc145544fca6b76e3bb4f720cb
Austin Jones <jonesaustindev@gmail.com>
2023-02-28 21:51:55 -0600

    working through hurtbox invincibility

--- Overlap/Hurtbox.gd
+++ Overlap/Hurtbox.gd
@@ -1,12 +1,30 @@
 extends Area2D
 
-export(bool) var show_hit = true
-
 const HitEffect = preload("res://Effects/HitEffect.tscn")
 
-func _on_Hurtbox_area_entered(area):
-	if show_hit:
-		var effect = HitEffect.instance()
-		var main = get_tree().current_scene
-		main.add_child(effect)
-		effect.global_position = global_position
+signal invincibility_ended
+signal invincibility_started
+
+onready var timer = $Timer
+
+var invincible := false setget set_invincible
+
+func set_invincible(value : bool):
+	invincible = value
+	if invincible == true:
+		emit_signal("invincibility_started")
+	else:
+		emit_signal("invincibility_ended")
+
+func start_invincibility(duration : float):
+	self.invincible = true # self required to call the setter
+	timer.start(duration)
+
+func create_hit_effect():
+	var effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position
+
+func _on_Timer_timeout():
+	self.invincible = false # self required to call the setter
